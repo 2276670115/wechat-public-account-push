973c0029db72d4d6485051364af793c1ff68dc3b
2276670115 <51191982+2276670115@users.noreply.github.com>
2023-05-18 10:13:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,13 +13,13 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '960f43dbd82484f1ecce72f76fc0d127',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '济南',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogRkx5kf7clzwu6XEtizRvfzHFY0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2013-05-20' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2023-11-18' },
       ],
     },
   ],
